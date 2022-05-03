14c62957479aef1febe4cb3c23a895bf0479582b
suman <ronaldosuman97@gmail.com>
2022-05-03 13:47:27 +0530

    2.3.9

--- firebase/app.js
+++ firebase/app.js
@@ -1,9 +1,16 @@
 import { initializeApp } from "firebase/app";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from "@env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC_K6VWPetxqdi00jhQbkQ39muRCH8Z5Do",
-  authDomain: "vivid-a8eb0.firebaseapp.com",
-  projectId: "vivid-a8eb0",
-  storageBucket: "vivid-a8eb0.appspot.com",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "104452556493",
   appId: "1:104452556493:web:359926244d8a5d69e64b84",
   measurementId: "G-SYB6HZHS2H",
@@ -11,22 +18,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export default app;
-
-// const firebaseConfig2 = {
-//   apiKey: "AIzaSyC_K6VWPetxqdi00jhQbkQ39muRCH8Z5Do",
-//   authDomain: "vivid-a8eb0.firebaseapp.com",
-//   projectId: "vivid-a8eb0",
-//   storageBucket: "vivid-a8eb0.appspot.com",
-//   messagingSenderId: "104452556493",
-//   appId: "1:104452556493:web:359926244d8a5d69e64b84",
-//   measurementId: "G-SYB6HZHS2H",
-// };
-// const firebaseConfig1 = {
-//   apiKey: "AIzaSyAsPBcHHvRBQihslPJZdCnRiwuKnenywkQ",
-//   authDomain: "alles-179b0.firebaseapp.com",
-//   databaseURL: "https://alles-179b0-default-rtdb.firebaseio.com",
-//   projectId: "alles-179b0",
-//   storageBucket: "alles-179b0.appspot.com",
-//   messagingSenderId: "288298780579",
-//   appId: "1:288298780579:web:705950f91fb501aa003c16",
-// };
